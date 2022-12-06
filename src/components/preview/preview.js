8303cd517f7ccdd6fdeb1564bed1a3f296cc82a4
seanheinrichs <seanheinrichs@gmail.com>
2022-12-06 06:31:51 -0500

    remove

--- src/components/preview/preview.js
+++ src/components/preview/preview.js
@@ -7,15 +7,9 @@ const PreviewImage = styled('img')({
   maxWidth: '100%'
 });
 
-const gardenOfCalendarPoetryPages = [
-  9, 10, 11, 12, 13, 14, 15, 16, 19, 20, 21, 22, 23, 24, 25, 26, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,
-  43, 44, 47, 48, 49, 50
-];
+const gardenOfCalendarPoetryPages = [9, 10, 11, 12, 13, 14, 15, 16, 19];
 
-const anthonyReturnsPoetryPages = [
-  7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36,
-  37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50
-];
+const anthonyReturnsPoetryPages = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 
 const generateRandomNumber = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
